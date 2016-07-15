be1249baf0551eb3683b8354cd91d5f51cfe2892
evanmalmud <evanmalmud@gmail.com>
2016-07-14 23:50:37 -0400

    Initial Showdown Work
    Wrote Initial Structs and half the csv parser.

---
+++ Grid_System/Assets/Scripts/CSVReader.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO;
+
+public class CSVReader : MonoBehaviour {
+
+    public TestAsset csvFile;
+
+    public Player[] players;
+    private char lineSeperater = '\n';
+    private char fieldSeperater = ',';
+
+    // Use this for initialization
+    void Start () {
+        List<string> header = new List<string>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void readData()
+    {
+        List<string> tempPlayer = new List<string>();
+        string[] records = csvFile.text.Split(lineSeperater);
+        bool hasHeader = false;
+
+        foreach (string record in records)
+        {
+            string[] fields = record.Split(fieldSeperator);
+            foreach (string field in fields)
+            {
+                if (!hasHeader)
+                {
+                    header.Add(field);
+                }
+                else
+                {
+                    //contentArea.text += field + "\t";
+                }
+            }
+
+            if (!hasHeader)
+            {
+                hasHeader = true;
+            }
+        }
+    }
+// Add data to CSV file
+}
---
+++ Grid_System/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+}
+
+struct PlayerData
+{
+    string Name;
+    POSITION position;
+    bool rightHanded;
+    int playerValue;
+    BATTING batting;
+}
+
+public struct BATTING
+{
+    int[] OutPU;
+    int[] OutSO;
+    int[] OutGB;
+    int[] OutFB;
+    int[] Walk;
+    int[] Single;
+    int[] SinglePus;
+    int[] Double;
+    int[] Triple;
+    int[] Homer;
+}
+
+public enum POSITION
+{
+    [Description("Catcher")]
+    eC,
+    [Description("First Basemen")]
+    eOneB,
+    [Description("Second Basemen")]
+    eTwoB,
+    [Description("Third Basemen")]
+    eThreeB,
+    [Description("Short Stop")]
+    eSS,
+    [Description("Left Field")]
+    eLF,
+    [Description("Right Field")]
+    eRF,
+    [Description("Center Field")]
+    eCF,
+    [Description("Out Field")]
+    eOF,
+    [Description("Designated Hitter")]
+    eDH,
+    [Description("Pitcher")]
+    eP 
+}
+
+public enum PITCHER_TYPE
+{
+    eStarting,
+    eRelief,
+    eCloser
+}
+
+public struct PITCHER
+{
+    PlayerData playerData;
+    PITCHER_TYPE pitcherType;
+    int inningsPitched;
+}
+
+public class Batter : Player
+{
+    PlayerData playerData;
+    int speed;
+    int strategyValue;
+}
